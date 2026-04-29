36d5affdfa5209cd9c60f1596619e13c1f5ab8f5
Harith Fakrullah <rith@Riths-MacBook-Air.local>
2026-04-29 16:08:06 +0800

    Edit template card

--- index.js
+++ index.js
@@ -46,16 +46,42 @@ server.listen(8000, '127.0.0.1', () => { //listen to port 8000
 /////////////////////////////////////
 //URLs (Hosting)
 //synchronous way
+const replaceTemplate = (temp, product) => {  //function to replace template with product data
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); 
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
+const tempOverview = fs.readFileSync('./starter/templates/template-overview.html', 'utf-8');
+const tempProduct = fs.readFileSync('./starter/templates/template-product.html', 'utf-8');
+const tempCard= fs.readFileSync('./starter/templates/template-card.html', 'utf-8');
+
 const data = fs.readFileSync('./starter/dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
-
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview');
+      res.writeHead(200, { //menulis header response
+        'Content-type': 'text/html'  
+      })
+
+      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); //replace template card with product data
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml); //replace product cards with product data
+        res.end(output);
+        // Product page
     } else if (pathName === '/product') {
         res.end('This is the product');
+        // API
     } else if(pathName === '/apo'){
 
       //async way
